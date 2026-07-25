2517389716bc04a533aa5a5a57bdb92890bc4b76
PWT <pannawildtouradmin@gmail.com>
2026-07-25 17:05:04 +0530

    Version

--- panna-wild-tour.php
+++ panna-wild-tour.php
@@ -1,11 +1,15 @@
 <?php
 /**
  * Plugin Name: Panna Wild Tour
+ * Description: This is a WordPress plugins for Wild Tours and travels agency.
+ * Version: 1.0.0
+ * Author: Panna Wild Tour
+ * Author URI: https://www.pannawildtour.com
  */
 
 defined('ABSPATH') || exit;
 
-define('PWT_VERSION', '1.0.1');
+define('PWT_VERSION', '1.0.0');
 define('PWT_PLUGIN_FILE', __FILE__);
 define('PWT_PLUGIN_PATH', plugin_dir_path(__FILE__));
 define('PWT_PLUGIN_URL', plugin_dir_url(__FILE__));
